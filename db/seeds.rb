93ff5cbbfdc20651b56e96a6f58a52c5a30f85d3
Corey McCann <mccann.australia@gmail.com>
2016-03-01 13:32:19 -0500

    Updated seeds.rb with colour and price

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create(name: 'Test Product', description: 'Lorem ipsum dolor sit amet, eos euripidis maiestatis instructior eu, id qui debitis fierent. Vix phaedrum consetetur te, sea inani accusamus an, te has scripta integre contentiones. Vel eu graeci audiam. Sed ei tation numquam.', image_url: 'https://source.unsplash.com/category/technology/500x220')
+Product.create(name: 'Test Product', colour: 'Black', price: 9.25, description: 'Lorem ipsum dolor sit amet, eos euripidis maiestatis instructior eu, id qui debitis fierent. Vix phaedrum consetetur te, sea inani accusamus an, te has scripta integre contentiones. Vel eu graeci audiam. Sed ei tation numquam.', image_url: 'https://source.unsplash.com/category/technology/500x220')
 
-Product.create(name: 'Another Cool Thing', description: 'At nostro albucius pro, veniam labores vel cu. Mei no nibh argumentum, eam cu assum aeque noster. Pro posse assum interpretaris no. No duo esse lucilius, per in civibus accusamus. Autem lorem nemore et mel. Sint wisi mandamus an per.', image_url: 'https://source.unsplash.com/category/food/500x220')
+Product.create(name: 'Another Cool Thing', colour: 'Purple', price: 22.50, description: 'At nostro albucius pro, veniam labores vel cu. Mei no nibh argumentum, eam cu assum aeque noster. Pro posse assum interpretaris no. No duo esse lucilius, per in civibus accusamus. Autem lorem nemore et mel. Sint wisi mandamus an per.', image_url: 'https://source.unsplash.com/category/food/500x220')
 
 #Product.create(name: '', description: '', image_url: '')
