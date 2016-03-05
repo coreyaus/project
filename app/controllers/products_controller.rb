1bc94c19fc8a6abee0934fa31776ffadf2832a31
Corey McCann <mccann.australia@gmail.com>
2016-03-04 21:56:05 -0500

    Added a search feature that filters the product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,16 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      if Rails.env.production?
+        @products = Product.where("name ilike ?", "%#{search_term}%")
+      else
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      end
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
