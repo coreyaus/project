8e9c51eb963feee00361993e51467d3af8c978db
Corey McCann <mccann.australia@gmail.com>
2016-03-04 19:34:34 -0500

    Changed static pages routes and refactored some code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,12 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+	def about
+	end
+
+	def contact
+	end
+
   def landing_page
     @featured_product = Product.first
     @products = Product.limit(3)
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
+  get 'index' => 'static_pages#index'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'static_pages#index'
   root 'static_pages#landing_page'
 
   # Example of regular route:
