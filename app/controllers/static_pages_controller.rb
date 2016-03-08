5678c1423862dc51da7c847232efdb937ecffd54
Corey McCann <mccann.australia@gmail.com>
2016-03-08 13:57:40 -0500

    Completing Exercise 5.3 and addig the mailer view template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,10 +14,11 @@ class StaticPagesController < ApplicationController
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  ActionMailer::Base.mail(:from => @email,
-	      :to => 'corey@codenation.com',
-	      :subject => "A new contact form message from #{@name}",
-	      :body => @message).deliver_now
+		# ActionMailer::Base.mail(from: @email,
+		# 	to: 'corey@codenation.com',
+		# 	subject: "A new contact form message from #{@name}",
+		# 	body: @message).deliver_now
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 
 end
